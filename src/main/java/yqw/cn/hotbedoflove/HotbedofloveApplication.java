835194ad04da2ecb1d92c1e44d0840f767db1d59
yqw <402481418@qq.com>
2020-06-04 18:02:36 +0800

    init

--- src/main/java/yqw/cn/hotbedoflove/HotbedofloveApplication.java
+++ src/main/java/yqw/cn/hotbedoflove/HotbedofloveApplication.java
@@ -1,9 +1,11 @@
 package yqw.cn.hotbedoflove;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("yqw.cn.hotbedoflove")
 public class HotbedofloveApplication {
 
     public static void main(String[] args) {
